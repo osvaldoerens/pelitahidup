60259086dbcc9e51c183d24af7d65cb247cc116d
osvaldoerens <osvaldoerens@rocketmail.com>
2020-02-26 15:47:19 +0700

    test commit intent

--- app/src/main/java/pelitahidup/gpib/pelitahidup/MainActivity.java
+++ app/src/main/java/pelitahidup/gpib/pelitahidup/MainActivity.java
@@ -1,13 +1,17 @@
 package pelitahidup.gpib.pelitahidup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class    MainActivity extends AppCompatActivity {
+public class    MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
